47d6b9e9ed916af8aa8a64a89ef2b83d15b8fa4e
csquires <chandlersquires18@gmail.com>
2018-06-27 14:49:24 -0400

    added new save utils

--- sys_utils.py
+++ sys_utils.py
@@ -1,14 +1,31 @@
 import os
+from scipy import sparse
 
 
-def ensure_dirs(paths):
-    print(paths)
-    if isinstance(paths, list):
-        for path in paths:
-            ensure_dirs(path)
-    else:
-        if not os.path.exists(os.path.dirname(paths)):
-            os.makedirs(paths)
+# DECORATORS
+def save_class(filename, saver, loader):
+    def save_dec(f):
+        def wrapper(self, *args, **kwargs):
+            if callable(filename):
+                signature = inspect.signature(f)
+                kwargs_ = {
+                    k: v.default
+                    for k, v in signature.parameters.items() if v.default is not inspect.Parameter.empty
+                }
+                kwargs_.update(kwargs)
+                full_filename = self.data_folder + filename(kwargs_)
+            else:
+                full_filename = self.data_folder + filename
+            if os.path.exists(full_filename):
+                return loader(full_filename)
+            else:
+                print('Generating %s' % full_filename)
+                res = f(self, *args, **kwargs)
+                print('Saving %s' % full_filename)
+                saver(res, full_filename)
+                return res
+        return wrapper
+    return save_dec
 
 
 def save(save, load):
@@ -26,6 +43,54 @@ def save(save, load):
         return wrapper
 
     return save_decorator
+
+
+# READ/WRITE
+def sparse_save(sparse_mat, filename):
+    save_types = (sparse.csc_matrix, sparse.csr_matrix, sparse.bsr_matrix, sparse.dia_matrix, sparse.coo_matrix)
+    if not isinstance(sparse_mat, save_types):
+        sparse_mat = sparse_mat.tocoo()
+    sparse.save_npz(filename, sparse_mat)
+
+
+def np_save(mat, filename):
+    np.save(filename, mat)
+
+
+def write_list(l, filename):
+    with open(filename, 'w') as f:
+        for item in l:
+            f.write('%s\n' % item)
+
+
+def read_list(filename):
+    l = []
+    with open(filename) as f:
+        for line in f.readlines():
+            l.append(line.rstrip())
+    return l
+
+
+def write_set(s, filename):
+    write_list(s, filename)
+
+
+def read_set(s, filename):
+    s = set()
+    with open(filename) as f:
+        for line in f.readlines():
+            s.add(line.rstrip())
+    return s
+
+
+# OTHER
+def ensure_dirs(paths):
+    if isinstance(paths, list):
+        for path in paths:
+            ensure_dirs(path)
+    else:
+        if not os.path.exists(os.path.dirname(paths)):
+            os.makedirs(paths)
 
 
 if __name__ == '__main__':
