64757a69270c8aff6a315176197aaf754fb6d429
Chandler <csquires@mit.edu>
2020-06-05 11:33:44 -0400

    remove print_iteration and add random_max

--- utils.py
+++ utils.py
@@ -1,10 +1,18 @@
+import operator as op
+import random
 
 
 def ix_map_from_list(l):
     return {e: i for i, e in enumerate(l)}
 
 
-def print_iteration(i, wait, message='Iteration {:,}'):
-    if i % wait == 0:
-        print(message.format(i))
-
+def random_max(d: dict, minimize=False):
+    """
+    Return a random key amongst the items in a dictionary with the maximum value.
+    """
+    if minimize:
+        optimal_val = min(d.items(), key=op.itemgetter(1))[1]
+    else:
+        optimal_val = max(d.items(), key=op.itemgetter(1))[1]
+    eligible_keys = [key for key, val in d.items() if val == optimal_val]
+    return random.choice(eligible_keys)
